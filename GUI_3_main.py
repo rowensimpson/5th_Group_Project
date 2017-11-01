c73c65b818fed54ed956ca2274a4dd1c628380db
Rowen Simpson <tam.shaw@hotmail.co.uk>
2017-11-01 17:27:46 +0000

    Add files via upload

--- GUI_3_main.py
+++ GUI_3_main.py
@@ -1,75 +1,119 @@
-from tkinter import *
+from __future__ import print_function
+import matplotlib
+import numpy as np
+
+matplotlib.use("TkAgg")
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+
+import tkinter as tk
 
 
-# StartPlane characteristics
-StartPlane = Tk()
-StartPlane.geometry('300x140')
-StartPlane.title("Micro-grid control centre")
+a = Figure(figsize=(4, 4))
+plot_a = a.add_subplot(111)
+plot_a.set_xlim([0, 2*np.pi])
+plot_a.set_ylim([-1, 1])
+lna, = plot_a.plot([], [], color='orange', lw=5)
+
+b = Figure(figsize=(4, 4))
+plot_b = b.add_subplot(111)
+plot_b.set_xlim([0, 2*np.pi])
+plot_b.set_ylim([-1, 1])
+
+lnb, = plot_b.plot([], [], color='olive', lw=5)
+
+x = 1
 
 
-def mode_button_1_func():
-    print("mode_button_1_func")
-
-
-def mode_button_2_func():
-    print("mode_button_2_func")
-
-
-def mode_button_3_func():
-    print("mode_button_3_func")
-
-
-def mode_exe_func():
-    print("mode_exe_func")
-
-
-def unit1_call():
-    print("unit1_clicked")
-    unit_1_level = Toplevel()
-    unit_1_level.title("Unit 1 viewer")
-
-
-def unit2_call():
-    print("unit2_clicked")
-    unit_2_level = Toplevel()
-    unit_2_level.title("Unit 2 viewer")
-
-
-def unit3_call():
-    print("unit3_clicked")
-    unit_3_level = Toplevel()
-    unit_3_level.title("Unit 3 viewer")
-
-
-def unit4_call():
-    print("unit4_clicked")
-    unit_4_level = Toplevel()
-    unit_4_level.title("Unit 4 viewer")
-
-
-def mode_call():
-    print("mode_clicked")
-    mode_level = Toplevel()
-    mode_level.geometry("200x200")
-    mode_level.title("Mode selection")
-    mode_button_1 = Button(mode_level, text="mode 1", command=mode_button_1_func).pack()
-    mode_button_2 = Button(mode_level, text="mode 2", command=mode_button_2_func).pack()
-    mode_button_3 = Button(mode_level, text="mode 3", command=mode_button_3_func).pack()
-    mode_exe = Button(mode_level, text="execute", command=mode_exe_func).pack()
-
-
-# button init
-Unit1 = Button(StartPlane, text="Unit 1", command=unit1_call).place(x=10, y=10)
-Unit2 = Button(StartPlane, text="Unit 2", command=unit2_call).place(x=10, y=40)
-Unit3 = Button(StartPlane, text="Unit 3", command=unit3_call).place(x=10, y=70)
-Unit4 = Button(StartPlane, text="Unit 4", command=unit4_call).place(x=10, y=100)
-Mode = Button(StartPlane, text="Select Mode", command=mode_call).place(x=70, y=25)
+def updateGraphsA(i):
+    pullData = open('sampleText.txt', 'r').read()
+    lna.set_xdata(x + i)
+    lna.set_ydata(x + i)
+    print('in A')
 
 
 
-# Label init
-txt = StringVar()
-Mode_Label = Label(StartPlane, text="Current Mode", anchor=CENTER, bg="red").place(x=70, y=55)
-Mode_Txt = Label(StartPlane, text=txt, anchor=CENTER, bg="blue").place(x=80, y=80)
+def updateGraphsB(i):
+    pullData = open('sampleText.txt', 'r').read()
+    lnb.set_xdata(x)
+    lnb.set_ydata(x)
+    print('in B')
 
-StartPlane.mainloop()
+
+
+class TransientAnalysis(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        self._running_anim = None
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, "Transient Analysis GUI: v1.0")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (GraphPageA, GraphPageB):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(GraphPageA)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+        frame.canvas.draw_idle()
+
+
+class GraphPageA(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        button1 = tk.Button(self, text="Show Graph B",
+                             command=(
+                                 lambda: controller.show_frame(GraphPageB)))
+        button1.grid(row=1, column=0, pady=20, padx=10, sticky='w')
+
+        canvasA = FigureCanvasTkAgg(a, self)
+        canvasA.show()
+        canvasA.get_tk_widget().grid(
+            row=1, column=1, pady=20, padx=10, sticky='nsew')
+        self.canvas = canvasA
+
+
+class GraphPageB(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        button1 = tk.Button(self, text="Show Graph A",
+                             command=(
+                                 lambda: controller.show_frame(GraphPageA)))
+
+        button1.grid(row=1, column=0, pady=20, padx=10, sticky='w')
+
+        canvasB = FigureCanvasTkAgg(b, self)
+        canvasB.show()
+        canvasB.get_tk_widget().grid(
+            row=1, column=1, pady=20, padx=10, sticky='nsew')
+        self.canvas = canvasB
+
+
+
+app = TransientAnalysis()
+app.geometry("800x600")
+aniA = animation.FuncAnimation(a, updateGraphsA, interval=1000, blit=False)
+aniB = animation.FuncAnimation(b, updateGraphsB, interval=1000, blit=False)
+app.mainloop()
