e33eb6893fb94b2375d12158fff4d98f14957993
Rowen Simpson <tam.shaw@hotmail.co.uk>
2017-11-04 20:49:38 +0000

    Add files via upload

---
+++ GUI_Header.py
@@ -0,0 +1,18 @@
+import random
+from time import sleep
+
+open('sampleText2.txt', 'w').close()
+
+for i in range(int(input("how much data?: "))):
+    afile = open("sampleText2.txt", "a")
+    afile.write(str(i))
+    for i1 in range(12):
+        afile.write(',')
+        line = str(random.randint(0, 50))
+        afile.write(line)
+        print(line)
+    afile.write('\n')
+    afile.close()
+    sleep(0.5)
+
+
